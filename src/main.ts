c0a592560ee02676c75a3a8449cbd4797ba7ae79
Guillaume L <guillaume@skillup.co>
2019-04-24 10:13:54 +0200

    [ADD] getHikingUrls models

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { getRegionUrls } from './models/scraper'
+import 'dotenv/config'
+
+import { getRegionUrls, getHikingUrls } from './models/scraper'
+// import { sleep } from './helpers/jsdom'
+
+// const { SLEEP } = process.env
 
 const main = async (): Promise<void> => {
   const regionUrls = await getRegionUrls()
+  const hikingUrls = await getHikingUrls(regionUrls[0])
 
-  console.log(regionUrls)
+  console.log(hikingUrls)
 
   process.exit(0)
 }
--- src/models/scraper.ts
+++ src/models/scraper.ts
@@ -1,15 +1,30 @@
+import { isEmpty } from 'lodash'
+
 import { extractBody } from '../helpers/jsdom'
 
-const getRegionUrls = async () => {
+const getRegionUrls = async (): Promise<string[]> => {
   const BASE_URL = 'https://www.visorando.com/'
   const document = await extractBody(BASE_URL)
-  const scope = document.querySelectorAll('.content-col .module .content-module > div > a')
+  const scope = document.querySelectorAll('.content-col .module .content-module ul > li > a')
 
   return Array
     .from(scope)
-    .map((element: Element)=> element.getAttribute('href'))
+    .map((element: Element) => element.getAttribute('href') || '')
+    .filter(url => !isEmpty(url))
+}
+
+const getHikingUrls = async (regionUrl: string): Promise<string[]> => {
+  const document = await extractBody(regionUrl)
+  const scope = document.querySelectorAll('.content-innerContentWithHeader .rando .rando-title-sansDetail > a')
+
+  return Array
+    .from(scope)
+    .map((element: Element) => element.getAttribute('href') || '')
+    .filter(url => !isEmpty(url))
+    .map(url => url.trim())
 }
 
 export {
   getRegionUrls,
+  getHikingUrls,
 }
