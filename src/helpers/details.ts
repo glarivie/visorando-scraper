3a66ef0a2429d13386a7873a402cd9794f50b4fc
Guillaume L <guillaume@skillup.co>
2019-04-25 12:34:46 +0200

    [FIX] reference regex

--- src/helpers/details.ts
+++ src/helpers/details.ts
@@ -1,12 +1,29 @@
 import { get, isEqual } from 'lodash'
 
-// interface IDetails {
-//   [key: string]: string;
-// }
+interface IDetails {
+  reference?: string;
+  duration?: string;
+  distance: number;
+  vertical: {
+    rise: number;
+    drop: number;
+  };
+  altitude: {
+    high: number;
+    low: number;
+  };
+  difficulty?: string;
+  loop: boolean;
+  type: string;
+  region?: string;
+  city?: string;
+  zipCode?: number;
+  coordinate?: string[];
+}
 
-const parseDetails = (details: string): any => {
+const parseDetails = (details: string): IDetails => {
   const results = {
-    reference: get(details.match(/Ref.\s+(\d+ET)/), '[1]'),
+    reference: get(details.match(/Ref.\s+(\d+[A-Z]{2})/), '[1]'),
     duration: get(details.match(/Durée moyenne:\s+(\d+h\d{2})\[\?\]/), '[1]'),
     distance: parseFloat(get(details.match(/Distance:\s+(\d+.?\d?\d?)km/), '[1]', 0)),
     vertical: {
@@ -19,7 +36,7 @@ const parseDetails = (details: string): any => {
     },
     difficulty: get(details.match(/Difficulté:\s+(\w+)/), '[1]'),
     loop: isEqual(get(details.match(/Retour point de départ:\s+([Oo]ui|[Nn]on)/), '[1]'), 'Oui'),
-    type: get(details.match(/(A pied|A VTT|En raquettes à neige|En cyclo-route)/), '[0]'),
+    type: get(details.match(/(A pied|A VTT|En raquettes à neige|En cyclo-route)/), '[0]', 'A pied'),
     region: get(details.match(/Région:\s+(.+) Commune/), '[1]'),
     city: get(details.match(/Commune:\s+(.+)\s\(\d+\)/), '[1]'),
     zipCode: parseInt(get(details.match(/Commune:\s+.+\s\((\d+)\)/), '[1]'), 10),
