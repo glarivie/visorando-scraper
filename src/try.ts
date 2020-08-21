50dcec2114b8cce293e4e917092c6613d1200439
Guillaume L <guillaume@skillup.co>
2020-08-21 10:41:39 +0200

    [TRY] another url

--- src/try.ts
+++ src/try.ts
@@ -4,7 +4,7 @@ import { getHikingDetails } from './models/scraper'
 // import { extractWaypoints } from './models/waypoints'
 
 const main = async (): Promise<void> => {
-  const url = 'https://www.visorando.com/randonnee-boucle-autour-du-pout/'
+  const url = 'https://www.visorando.com/randonnee-le-lac-branlay/'
   const hiking = await getHikingDetails(url)
 
   // const waypoints = await extractWaypoints(hiking.id)
